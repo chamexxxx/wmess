a4e5891d9d335a13dcac3ba568906a0933f5c61a
Vyacheslav Vatlin <vyacheslav.vatlin@gmail.com>
2026-06-20 21:13:39 +0400

    add axios interceptors for redirect to login

--- WMess.Client/src/api/index.ts
+++ WMess.Client/src/api/index.ts
@@ -1,8 +1,29 @@
+import { isAxiosError } from 'axios'
+import type { AxiosInstance } from 'axios'
 import { Bff } from './generated/Bff'
 import { Projects } from './generated/Projects'
 import { Teams } from './generated/Teams'
 import { User } from './generated/User'
 import type { ApiConfig } from './generated/http-client'
+
+const AUTH_PATHS = ['/api/login', '/api/register']
+
+function attachAuthInterceptor(instance: AxiosInstance) {
+  instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (
+        isAxiosError(error) &&
+        error.response?.status === 401 &&
+        !AUTH_PATHS.includes(error.config?.url ?? '')
+      ) {
+        window.location.href = '/login'
+      }
+
+      return Promise.reject(error)
+    },
+  )
+}
 
 const config: ApiConfig = {
   withCredentials: true,
@@ -16,9 +37,15 @@ class WMessApiClient extends Bff {
 
   constructor(config: ApiConfig) {
     super(config)
+
     this.teams = new Teams(config)
     this.projects = new Projects(config)
     this.user = new User(config)
+
+    attachAuthInterceptor(this.instance)
+    attachAuthInterceptor(this.teams.instance)
+    attachAuthInterceptor(this.projects.instance)
+    attachAuthInterceptor(this.user.instance)
   }
 }
 
